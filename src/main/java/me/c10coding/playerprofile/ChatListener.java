37482b3b125f6ff55f439e7df56fd4abf66736a8
Caleb Owens <caleb.ja.owens@gmail.com>
2020-06-11 13:10:56 -0400

    Added Placeholder support

--- src/main/java/me/c10coding/playerprofile/ChatListener.java
+++ src/main/java/me/c10coding/playerprofile/ChatListener.java
@@ -1,8 +1,8 @@
 package me.c10coding.playerprofile;
 
-import lpsapi.lpsapi.LPSAPI;
 import me.c10coding.coreapi.CoreAPI;
 import me.c10coding.coreapi.chat.Chat;
+import me.clip.placeholderapi.PlaceholderAPI;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.cacheddata.CachedMetaData;
@@ -13,32 +13,35 @@ import net.luckperms.api.query.QueryOptions;
 import net.md_5.bungee.api.chat.*;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.File;
 import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 
 public class ChatListener implements Listener {
 
-    LPSAPI lpapi = (LPSAPI) Bukkit.getPluginManager().getPlugin("LPSAPI");
-    Economy e = PlayerProfile.getEconomy();
     PlayerProfile plugin;
+    CoreAPI api = new CoreAPI();
+    Chat chatFactory = api.getChatFactory();
 
     public ChatListener(PlayerProfile plugin){
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGHEST)
     public void onChat(AsyncPlayerChatEvent e){
+
         e.setCancelled(true);
         Player p = e.getPlayer();
-        String format = e.getFormat();
 
         LuckPerms lp = LuckPermsProvider.get();
         ContextManager contextManager = lp.getContextManager();
@@ -46,11 +49,21 @@ public class ChatListener implements Listener {
         ImmutableContextSet contextSet = contextManager.getContext(user).orElseGet(contextManager::getStaticContext);
         CachedMetaData metaData = user.getCachedData().getMetaData(QueryOptions.contextual(contextSet));
 
-        String prefix = metaData.getPrefix();
-        TextComponent profile = new TextComponent("<" + prefix + " "  + p.getName());
+        String prefix = "";
+        String playerName = p.getName();
+
+        if(metaData.getPrefix() != null){
+            prefix = chatFactory.chat(metaData.getPrefix() + " ");
+        }
+
+        if(!p.getDisplayName().equalsIgnoreCase(p.getName())){
+            playerName = p.getDisplayName();
+        }
+
+        TextComponent profile = new TextComponent("<" + chatFactory.chat(playerName));
         TextComponent msg = new TextComponent("> " + e.getMessage());
 
-        profile.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, getComponent(p)));
+        profile.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, getComponent(p, prefix)));
         profile.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + p.getName()));
         profile.addExtra(msg);
 
@@ -58,47 +71,71 @@ public class ChatListener implements Listener {
             pl.spigot().sendMessage(profile);
         }
 
-
     }
 
-    public BaseComponent[] getComponent(Player p){
+    public BaseComponent[] getComponent(Player p, String prefix){
 
-        CoreAPI api = new CoreAPI();
-        Chat chatFactory = api.getChatFactory();
-        String playerName = chatFactory.removeChatColor(p.getName());
-        String displayName = chatFactory.removeChatColor(p.getDisplayName());
-        int balance = (int) e.getBalance(p);
-        int level = lpapi.exp.checkLevel(p);
+        File f = new File("plugins/Essentials", "userdata/" + p.getUniqueId() + ".yml");
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(f);
+        String playerName = p.getName();
+        String nickname =  config.getString("nickname");
 
-        if(playerName.equalsIgnoreCase(displayName)){
-            displayName = "None";
+        if(nickname == null){
+            nickname = playerName;
         }
 
-        List<String> settingKeys = getSettingsKeys();
-        List<String> settingsValues = new ArrayList<>();
-        settingsValues.add(playerName);
-        settingsValues.add(displayName);
-        settingsValues.add(String.valueOf(balance));
-        settingsValues.add(String.valueOf(level));
+        List<String> lines = getLines();
 
         ComponentBuilder cb = new ComponentBuilder("");
-        for(int x = 0; x < settingKeys.size(); x++){
-            if(plugin.getConfig().getBoolean("Settings." + settingKeys.get(x))){
-                cb.append(settingKeys.get(x) + ": " + settingsValues.get(x));
-                if(x != settingKeys.size()-1){
-                    cb.append("\n");
-                }
+        for(int x = 0; x < getLines().size(); x++){
+            String line = lines.get(x);
+            line = PlaceholderAPI.setPlaceholders(p, line);
+
+            if(line.contains("%nickName%")){
+                line = line.replace("%nickName%", nickname);
+            }
+
+            if(line.contains("%balance%")){
+                line = line.replace("%balance%", format((long)PlayerProfile.getEconomy().getBalance(p)));
+            }
+
+            cb.append(line);
+            if(x != lines.size() - 1){
+                cb.append("\n");
             }
         }
 
         return cb.create();
     }
 
-    public List<String> getSettingsKeys(){
-        List<String> settingsKeys = new ArrayList<>();
-        ConfigurationSection cs = plugin.getConfig().getConfigurationSection("Settings");
-        cs.getKeys(false).stream().forEach(key -> settingsKeys.add(key));
-        return settingsKeys;
+    public List<String> getLines(){
+        return plugin.getConfig().getStringList("Lines");
     }
+
+    private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+    static {
+        suffixes.put(1_000L, "k");
+        suffixes.put(1_000_000L, "M");
+        suffixes.put(1_000_000_000L, "G");
+        suffixes.put(1_000_000_000_000L, "T");
+        suffixes.put(1_000_000_000_000_000L, "P");
+        suffixes.put(1_000_000_000_000_000_000L, "E");
+    }
+
+    public static String format(long value) {
+        //Long.MIN_VALUE == -Long.MIN_VALUE so we need an adjustment here
+        if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
+        if (value < 0) return "-" + format(-value);
+        if (value < 1000) return Long.toString(value); //deal with easy case
+
+        Map.Entry<Long, String> e = suffixes.floorEntry(value);
+        Long divideBy = e.getKey();
+        String suffix = e.getValue();
+
+        long truncated = value / (divideBy / 10); //the number part of the output times 10
+        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
+    }
+
 
 }
--- src/main/java/me/c10coding/playerprofile/PlayerProfile.java
+++ src/main/java/me/c10coding/playerprofile/PlayerProfile.java
@@ -1,21 +1,28 @@
 package me.c10coding.playerprofile;
 
+import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class PlayerProfile extends JavaPlugin {
 
     private static Economy econ = null;
+    private static Chat chat = null;
 
     @Override
     public void onEnable() {
-        this.saveResource("config.yml", false);
+        if(!new File(this.getDataFolder(), "config.yml").exists()){
+            this.saveResource("config.yml", false);
+        }
         if (!setupEconomy()) {
             this.getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
+        setupChat();
         this.getServer().getPluginManager().registerEvents(new ChatListener(this), this);
     }
 
@@ -34,6 +41,15 @@ public final class PlayerProfile extends JavaPlugin {
         }
         econ = rsp.getProvider();
         return econ != null;
+    }
+
+    private void setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+    }
+
+    public static Chat getChat() {
+        return chat;
     }
 
     public static Economy getEconomy() {
